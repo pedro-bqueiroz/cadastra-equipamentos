ecfadae253d5a21e5b313f1835e86990d8b61f31
Pedro Queiroz <pedrobirroqz@gmail.com>
2026-02-22 00:43:03 -0300

    Modify pause command for terminal window behavior
    Updated pause command to allow for automatic window closure.

--- Cadastra-Equipamentos/Autocompilador para Windows.bat
+++ Cadastra-Equipamentos/Autocompilador para Windows.bat
@@ -2,4 +2,4 @@
 gcc -c cadastra_equipamentos.c
 gcc cadastra_equipamentos.o Cadastra-Equipamentos.c -o Cadastra-Equipamentos.exe
 start Cadastra-Equipamentos.exe
-pause :: Útil para avisar sobre possíveis erros de compilação, mas a janela do terminal fica aberta. Coloque pause depois dos 4 pontos para fechar a janela automaticamente 
+pause & :: Útil para avisar sobre possíveis erros de compilação, mas a janela do terminal fica aberta. Coloque pause depois dos 4 pontos para fechar a janela automaticamente 
