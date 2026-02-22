48abd183d66700d0c1db991f9c37fdb4c271dc1e
Pedro Queiroz <pedrobirroqz@gmail.com>
2026-02-21 22:27:51 -0300

    Delete Autocompilador WINDOWS.bat

--- Autocompilador WINDOWS.bat
+++
@@ -1,5 +0,0 @@
-:: É necessário já ter o GCC instalado e no PATH. A implementação de um 'autocompilador' para pessoas que não têm tanto conhecimento sobre tecnologia será revista no futuro, e pretendo que seja possível apenas clicar duas vezes neste arquivo.
-gcc -c cadastra_equipamentos.c
-gcc cadastra_equipamentos.o cadastra-equipamentos.c -o Cadastra-Equipamentos.exe
-start Cadastra-Equipamentos.exe
-pause
