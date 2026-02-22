d0c87b8cb63a3d5d1e7ab00fb7a38cb62905f074
Pedro Queiroz <pedrobirroqz@gmail.com>
2026-02-21 23:41:59 -0300

    Correct compilation command and improve pause message
    Fix file name in compilation command and update pause comment.

--- Cadastra-Equipamentos/Autocompilador para Windows.bat
+++ Cadastra-Equipamentos/Autocompilador para Windows.bat
@@ -1,5 +1,5 @@
 :: É necessário já ter o GCC instalado e no PATH. A implementação de um 'autocompilador' para pessoas que não têm tanto conhecimento sobre tecnologia será revista no futuro, e pretendo que seja possível apenas clicar duas vezes neste arquivo.
 gcc -c cadastra_equipamentos.c
-gcc cadastra_equipamentos.o cadastra-equipamentos.c -o Cadastra-Equipamentos.exe
+gcc cadastra_equipamentos.o Cadastra-Equipamentos.c -o Cadastra-Equipamentos.exe
 start Cadastra-Equipamentos.exe
-pause
+pause :: Útil para avisar sobre possíveis erros de compilação, mas a janela do terminal fica aberta. Coloque pause depois dos 4 pontos para fechar a janela automaticamente 
