55157eda116ecb00647d60cdcd6c81a22059c03b
Pedro Queiroz <pedrobirroqz@gmail.com>
2026-03-21 23:53:49 -0300

    Update

--- src/Cadastra-Equipamentos.c
+++ src/Cadastra-Equipamentos.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+#include <sys/stat.h>
 #include "cadastra_equipamentos.h"
 
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, "Portuguese"); // Configura a localidade para Português do Brasil, garantindo a exibição correta de caracteres acentuados.
-    system("cls || clear"); // útil para limpar a tela em sistemas Windows e Unix, respectivamente.
+    setlocale(LC_NUMERIC, "C"); // Evita a conversão de pontos decimais para vírgulas, facilitando a impressão de dados para arquivos CSV.
+    system("cls || clear"); // Útil para limpar a tela em sistemas Windows e Unix, respectivamente.
 
-    FILE *arq = fopen("equipamentos.txt", "r"); // será feito um teste de existência do arquivo.
-    int n=0; //variável que vai fazer a contagem de quantos equipamentos estão cadastrados, importante para se usar nos loops.
+    FILE *arq = fopen("equipamentos.txt", "r"); // Será feito um teste de existência do arquivo.
+    int n=0; // Variável que vai fazer a contagem de quantos equipamentos estão cadastrados, importante para se usar nos loops.
     int confirmacao;
-    Equipamentos vetor[MAX_EQUIPAMENTOS]; //Vetor responsável por armazenar os equipamentos já cadastrados.
+    Equipamentos vetor[MAX_EQUIPAMENTOS]; // Vetor responsável por armazenar os equipamentos já cadastrados.
 
     int nivelAcesso = loginSenha();
     if (nivelAcesso == -1) {
@@ -20,7 +22,7 @@ int main(int argc, char *argv[]) {
 
     fprintf(stdout, "\nBem vindo ao Cadastra-Equipamentos.\nCertifique-se de que fechou o programa quando for embora!!!\n");
 
-    while (arq == NULL){ //testa se o arquivo existe ou não
+    while (arq == NULL){ // Testa se o arquivo existe ou não
         fprintf(stderr, "ATENÇÃO! Não há arquivo .txt a ser aberto. Deseja criar um? S/N ");
         scanf("\n%c", &confirmacao);
         if (confirmacao == 'n' || confirmacao == 'N'){
@@ -52,47 +54,72 @@ int main(int argc, char *argv[]) {
         fprintf(stdout, "8) Carregar dados do arquivo\n");
         fprintf(stdout, "9) Configurações de login e senha\n");
         fprintf(stdout, "0) Listar usuários\n");
+        fprintf(stdout, "Q) Verificar alterações\n");
+        fprintf(stdout, "W) Importar ou exportar arquivos em outros formatos\n");
         fprintf(stdout, "C) Sair do programa\n\n");
 
         scanf(" %c", &input);
-        switch (input){
+        switch (input){ 
             case '1':
                 adicionarEquipamento(vetor, &n, nivelAcesso);
                 break;
+
             case '2':
                 editarEquipamento(vetor, &n, nivelAcesso);
                 break;
+
             case '3':
                 removerEquipamento(vetor, &n, nivelAcesso);
                 break;
+
             case '4':
                 listarEquipamentos(vetor, &n, nivelAcesso);
                 break;
+                
             case '5':
                 buscarPorFabricante(vetor, &n, nivelAcesso);
                 break;
+
             case '6':
                 listarEquipamentosPorUso(vetor, &n, nivelAcesso);
                 break;
+
             case '7':
                 salvarDadosEmArquivo(vetor, &n, nivelAcesso);
                 break;
+
             case '8':
                 carregarDadosDoArquivo(vetor, &n, nivelAcesso);
                 break;
+
             case '9':
                 configurarLoginSenha(nivelAcesso);
                 break;
+
             case '0':
                 listarUsuarios(nivelAcesso);
                 break;
+
+            case 'q':
+            case 'Q':
+                verificarAlteracoes(vetor, &n, nivelAcesso);
+                break;
+
+            case 'w':
+            case 'W':
+                abrirOutrosFormatos(vetor, &n, nivelAcesso);
+                break;
+
             case 'c':
             case 'C':
                 fprintf(stdout, "Saindo do programa...");
                 break;
+
             default:
                 fprintf(stderr, "Alternativa inválida.\n");
                 break;
         }
     } while (input != 'c' && input != 'C');
+
+    return 0;
 }
